cb9969b02c6875aa5b25444ba4b03d1504101cd2
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2023-11-22 15:48:56 +0530

    my account

--- src/Pages/MyAccount.jsx
+++ src/Pages/MyAccount.jsx
@@ -1,15 +1,56 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 import Head from '../Components/Head'
+import TextField from '@mui/material/TextField';
+import Form from 'react-bootstrap/Form';
 
 function MyAccount() {
   return (
     <>
-    <Head/>
+      <Head />
       <Row className='d-flex flex-row'>
         <Col lg={7} className='mt-5 ms-5 me-5 pe-4'>
-         <h3>Update Youre Account</h3>
-         
+          <h1 style={{color:'pink',fontWeight:'400'}} className='mb-5'>Update Youre Account</h1>
+          <div className='d-flex flex-column'>
+            <h3 style={{fontFamily:'serif'}} className='mb-3 '>Profile Picture</h3>
+            <label>
+              <input type="file" style={{ display: 'none' }} o />
+              <img className='img-fluid' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJiT-UHSm6w0Jperb8SitpfoAKeMUE3uynPg5YO-2Drw&s" alt="" />
+            </label>
+            <Form className='mt-5'>
+            <Form.Group className="mb-3" >
+                <Form.Label className='fw-bold'>User Name</Form.Label>
+                <Form.Control type="text" placeholder='Enter User Name' />
+                </Form.Group>
+              <Form.Group className="mb-3" >
+                <Form.Label className='fw-bold'>Email address</Form.Label>
+                <Form.Control type="email" placeholder='Enter Email' />
+                </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label className='fw-bold'>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              
+              <Form.Group className="mb-3" >
+                <Form.Label className='fw-bold'>Catogeries</Form.Label>
+                <Form.Control type="text" />
+                </Form.Group>
+                <Form.Group className="mb-3" >
+                <Form.Label className='fw-bold'>Facebook Link</Form.Label>
+                <Form.Control type="text" />
+                               </Form.Group>
+                               <Form.Group className="mb-3" >
+                <Form.Label className='fw-bold'>Instegram Link</Form.Label>
+                <Form.Control type="text" />
+                </Form.Group>
+                <Form.Group className="mb-3" >
+                <Form.Label className='fw-bold'>Twitter Link</Form.Label>
+                <Form.Control type="text" />
+                </Form.Group>
+            </Form>
+<div className='d-flex justify-content-center alighn-items-center mt-5'><button className='btn btn-success w-50'>Update</button></div>
+          </div>
         </Col>
         <Col lg={3} className='ms-5 mt-5'>
           <div style={{ width: '450px', height: '3px', backgroundColor: 'black' }}></div>
