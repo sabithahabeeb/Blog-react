f23dea6034acc3dfe664de7a37fb988102ac528a
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2023-12-03 06:36:32 +0530

    my acoount

--- src/Components/Head.jsx
+++ src/Components/Head.jsx
@@ -7,38 +7,38 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link, useNavigate } from 'react-router-dom';
 
-function Head() {
- 
-  const [username,setUsername] = useState("")
+function Head({ account }) {
+
+  const [username, setUsername] = useState("")
   const navigateURL = useNavigate()
-  const navigate = ()=>{
+  const navigate = () => {
     navigateURL('/myaccount')
   }
- useEffect(()=>{
-if(sessionStorage.getItem("existingUser")){
-setUsername(JSON.parse(sessionStorage.getItem("existingUser")).username)
+  useEffect(() => {
+    if (sessionStorage.getItem("existingUser")) {
+      setUsername(JSON.parse(sessionStorage.getItem("existingUser")).username)
 
-}
- },[])
+    }
+  }, [])
 
- const handleLogout = ()=>{
-  sessionStorage.removeItem("existingUser")
-  sessionStorage.removeItem("token")
-  navigateURL('/')
- }
+  const handleLogout = () => {
+    sessionStorage.removeItem("existingUser")
+    sessionStorage.removeItem("token")
+    navigateURL('/')
+  }
   return (
     <>
-        <Navbar  expand="lg" className="bg-body-tertiary  ">
-      <Container fluid>
-      <img
-              alt=""
-              src="https://cdn.icon-icons.com/icons2/1571/PNG/512/483490-blogger-google-logo-media-network-social_107708.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
+      <Navbar expand="lg" className="bg-body-tertiary  ">
+        <Container fluid>
+          <img
+            alt=""
+            src="https://cdn.icon-icons.com/icons2/1571/PNG/512/483490-blogger-google-logo-media-network-social_107708.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          <Navbar.Toggle aria-controls="navbarScroll" />
+
           <Nav
             className="me-auto my-2 my-lg-0"
             style={{ maxHeight: '100px' }}
@@ -50,27 +50,27 @@ setUsername(JSON.parse(sessionStorage.getItem("existingUser")).username)
             <Nav.Link href="#" >
             CONTACT
             </Nav.Link> */}
-            <Nav.Link  href="/write">WRITE</Nav.Link>
+            <Nav.Link href="/write">WRITE</Nav.Link>
 
           </Nav>
-         <div onClick={navigate} className='d-flex flex-row justify-content-center'>
+          {!account && <div onClick={navigate} className='d-flex flex-row justify-content-center'>
             <img
-                alt=""
-                src="https://cdn.icon-icons.com/icons2/1571/PNG/512/483490-blogger-google-logo-media-network-social_107708.png"
-                width="40"
-                height="40"
-                className="d-inline-block align-top"
-              />{' '}
-            <Nav.Link className='fw-bold text-warning' href="#action2" style={{margin:'20px',fontSize:'20px'}}>{username}</Nav.Link>
-         </div>
+              alt=""
+              src="https://cdn.icon-icons.com/icons2/1571/PNG/512/483490-blogger-google-logo-media-network-social_107708.png"
+              width="40"
+              height="40"
+              className="d-inline-block align-top"
+            />{' '}
+            <Nav.Link className='fw-bold text-warning' href="#action2" style={{ margin: '20px', fontSize: '20px' }}>{username}</Nav.Link>
+          </div>}
 
           {/* <Nav.Link href="#action2">LOGOUT</Nav.Link> */}
-          <button onClick={handleLogout} style={{textDecoration:'none',color:'orange'}} className='btn btn-link fw-bolder fs-5'>LOGOUT</button>
+          <button onClick={handleLogout} style={{ textDecoration: 'none', color: 'orange' }} className='btn btn-link fw-bolder fs-5'>LOGOUT</button>
 
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  
+
+        </Container>
+      </Navbar>
+
     </>
   )
 }
--- src/Pages/MyAccount.jsx
+++ src/Pages/MyAccount.jsx
@@ -1,110 +1,119 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import Head from '../Components/Head'
-import TextField from '@mui/material/TextField';
 import Form from 'react-bootstrap/Form';
-import Write from '../Components/Write';
-import { useNavigate } from 'react-router-dom';
+import Collapse from 'react-bootstrap/Collapse';
+import Footer from '../Components/Footer';
+import { BASE_URL } from '../Services/baseurl';
 
 function MyAccount() {
-  const navigateURL = useNavigate()
-  const navigatewrite = ()=>{
-    navigateURL('/write')
-  }
+  const [open, setOpen] = useState(false);
+  const [useProfile, setUseProfile] = useState({
+    profile: "", username: "", email: "", password: "", about: "", facebook: "", instegram: "", twitter: ""
+  })
+  const [existingImage, setExistingImage] = useState("")
+  const [preview, setPreview] = useState("")
+  useEffect(() => {
+    const user = JSON.parse(sessionStorage.getItem("existingUser"))
+    setUseProfile({
+      ...useProfile, profile: "", username: user.username, email: user.email, password: user.password, about: user.aboutme, facebook: user.facebook, instegram: user.instegram, twitter: user.twitter
+    })
+    setExistingImage(user.profile)
+
+  }, [])
+  useEffect(() => {
+    if (useProfile.profile) {
+      setPreview(URL.createObjectURL(useProfile.profile))
+    } else {
+      setPreview("")
+    }
+  }, [useProfile.profile])
+
   return (
     <>
-      <Head />
+      <Head account />
       <Row className='d-flex flex-row'>
         <Col lg={5} className='mt-5 ms-5 me-5 pe-4'>
-          <h1 style={{color:'pink',fontWeight:'400'}} className='mb-5'>Update Youre Account</h1>
+          <h1 style={{ color: 'pink', fontWeight: '400' }} className='mb-5'>Update Youre Account</h1>
           <div className='d-flex flex-column card shadow w-100 ms-3 p-3' >
-            <h3 style={{fontFamily:'serif'}} className='mb-3 '>Profile Picture</h3>
+            <h3 style={{ fontFamily: 'serif' }} className='mb-3 '>Profile Picture</h3>
             <label>
-              <input type="file" style={{ display: 'none' }} o />
-              <img className='img-fluid' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJiT-UHSm6w0Jperb8SitpfoAKeMUE3uynPg5YO-2Drw&s" alt="" />
+              <input type="file" style={{ display: 'none' }} o onChange={e => setUseProfile({ ...useProfile, profile: e.target.files[0] })} />
+              {existingImage !== "" ?
+                <img width={'200px'} className='fluid mt-5 ms-5' src={preview ? preview : `${BASE_URL}/uploads/${existingImage}`} alt="upload profile" /> :
+                <img width={'200px'} className='fluid mt-5 ms-5' src={preview ? preview : `"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJiT-UHSm6w0Jperb8SitpfoAKeMUE3uynPg5YO-2Drw&s"`} alt="upload profile" />}
             </label>
             <Form className='mt-5'>
-            <Form.Group className="mb-3" >
+              <Form.Group className="mb-3" >
                 <Form.Label className='fw-bold'>User Name</Form.Label>
                 <Form.Control type="text" placeholder='Enter User Name' />
-                </Form.Group>
+              </Form.Group>
               <Form.Group className="mb-3" >
                 <Form.Label className='fw-bold'>Email address</Form.Label>
                 <Form.Control type="email" placeholder='Enter Email' />
-                </Form.Group>
+              </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label className='fw-bold'>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
               </Form.Group>
-              
-              {/* <Form.Group className="mb-3" >
-                <Form.Label className='fw-bold'>Catogeries</Form.Label>
-                <Form.Control type="text" />
-                </Form.Group> */}
-                <Form.Group className="mb-3" >
-                <Form.Label className='fw-bold'>Facebook Link</Form.Label>
-                <Form.Control type="text" />
-                               </Form.Group>
-                               <Form.Group className="mb-3" >
-                <Form.Label className='fw-bold'>Instegram Link</Form.Label>
-                <Form.Control type="text" />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                <Form.Label className='fw-bold'>Twitter Link</Form.Label>
-                <Form.Control type="text" />
-                </Form.Group>
+
+              <button onClick={() => setOpen(!open)} className='btn btn-success me-5'>More</button>
+              {/*  */}
+              <Collapse in={open}>
+                <div id="example-collapse-text">
+                  <Form.Group className="mb-3" >
+                    <Form.Label className='fw-bold'>About Me</Form.Label>
+                    <Form.Control type="text" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" >
+                    <Form.Label className='fw-bold'>Facebook Link</Form.Label>
+                    <Form.Control type="text" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" >
+                    <Form.Label className='fw-bold'>Instegram Link</Form.Label>
+                    <Form.Control type="text" />
+                  </Form.Group>
+                  <Form.Group className="mb-3" >
+                    <Form.Label className='fw-bold'>Twitter Link</Form.Label>
+                    <Form.Control type="text" />
+                  </Form.Group>
+                  <div className='d-flex justify-content-center alighn-items-center mt-5'><button className='btn btn-success w-50'>Update</button></div>
+
+                </div>
+              </Collapse>
+
+
             </Form>
-<div className='d-flex justify-content-center alighn-items-center mt-5'><button className='btn btn-success w-50'>Update</button></div>
+
           </div>
         </Col>
-        <Col></Col>
-        {/* <Col lg={4} className='ms-5 mt-5 '>
-         <h1>Add Youre Blog</h1>
-         <Write/>
-<div className='card shadow p-3 m-3'>
-  
-           <h2 className='m-5'>MY BLOGs</h2>
-           <table className=' '>
-           <thead>
-            <tr>
-              <th className='p-3'>#</th>
-              <th className='p-3'>Title</th>
-              <th className='p-3'>image</th>
-              <th className='p-3'>edit</th>
-             <th className='p-3'>Delete</th>
-            </tr>
-  
-          </thead>
-          <tbody>
-           <tr>
-              <td className='p-3'>1</td>
-              <td className='p-3'>dsfd</td>
-              <td className='p-3'><img width={'100px'} height={'100px'} className='img-fluid' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJiT-UHSm6w0Jperb8SitpfoAKeMUE3uynPg5YO-2Drw&s" alt="" /></td>
-              <td className='p-3'><button className="btn"><i className="fa-solid fa-pen-to-square "></i></button></td>
-              <td className='p-3'><button className='btn'><i className="fa-solid fa-trash text-danger"></i></button></td>
-           </tr>
-           <tr>
-              <td className='p-3'>1</td>
-              <td className='p-3'>dsfd</td>
-              <td className='p-3'><img width={'100px'} height={'100px'} className='img-fluid' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJiT-UHSm6w0Jperb8SitpfoAKeMUE3uynPg5YO-2Drw&s" alt="" /></td>
-              <td className='p-3'><button className="btn"><i className="fa-solid fa-pen-to-square "></i></button></td>
-              <td className='p-3'><button className='btn'><i className="fa-solid fa-trash text-danger"></i></button></td>
-           </tr>
-           <tr>
-              <td className='p-3'>1</td>
-              <td className='p-3'>dsfd</td>
-              <td className='p-3'><img width={'100px'} height={'100px'} className='img-fluid' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJiT-UHSm6w0Jperb8SitpfoAKeMUE3uynPg5YO-2Drw&s" alt="" /></td>
-              <td className='p-3'><button className="btn"><i className="fa-solid fa-pen-to-square "></i></button></td>
-              <td className='p-3'><button className='btn'><i className="fa-solid fa-trash text-danger"></i></button></td>
-           </tr>
-          </tbody>
-  
-           </table>
-      
-</div>
-        </Col> */}
+        <Col lg={1}></Col>
+        <Col lg={3} className='ms-5 mt-5'>
+          <div style={{ width: '400px', height: '3px', backgroundColor: 'black' }}></div>
+          <h2 className='text-center mt-3 ms-5 fw-bold'>About Me</h2>
+          <div style={{ width: '400px', height: '3px', backgroundColor: 'black' }}></div>
+          {existingImage !== "" ?
+            <img width={'200px'} className='fluid mt-5 ms-5' src={preview ? preview : `${BASE_URL}/uploads/${existingImage}`} alt="" /> :
+            <img width={'200px'} className='fluid mt-5 ms-5' src={preview ? preview : `"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJiT-UHSm6w0Jperb8SitpfoAKeMUE3uynPg5YO-2Drw&s"`} alt="" />}
+          <p className='mt-4 ms-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quis mollitia asperiores quibusdam excepturi repellat nisi atque sit praesentium ex eligendi provident exercitationem vero, maxime veritatis. Eaque, quo! Quaerat, tempora.</p>
+          {/* <div style={{ width: '450px', height: '3px', backgroundColor: 'black' }}></div>
+          <h2 className='text-center mt-3 ms-5 fw-bold'>Catagories</h2>
+          <div style={{ width: '450px', height: '3px', backgroundColor: 'black' }}></div>
+          <ul style={{ width: '30px' }} className='d-flex mt-5'>
+            <li style={{ textDecoration: 'none' }} className='m-2 fw-bolder'>food</li>
+          </ul> */}
+          <div style={{ width: '400px', height: '3px', backgroundColor: 'black' }}></div>
+          <h2 className='text-center mt-3 ms-5 fw-bold'>FOLLOW ME</h2>
+          <div style={{ width: '400px', height: '3px', backgroundColor: 'black' }}></div>
+          <div className='d-flex flex-row mt-5 justify-content-around alighn-items-center'>
+            <i style={{ fontSize: '30px' }} className="fa-brands fa-facebook-f"></i>
+            <i style={{ fontSize: '30px' }} className="fa-brands fa-twitter"></i>
+            <i style={{ fontSize: '30px' }} className="fa-brands fa-instagram"></i>
+          </div>
+        </Col>
       </Row>
+      <Footer />
     </>
   )
 }
